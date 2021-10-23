3b74dd3f5b64a6bcfb79bb3d224911f644993457
beauwilliams <beau.williams@icloud.com>
2021-10-23 12:31:27 +1100

    test

---
+++ Verification/truffle-config.js
@@ -0,0 +1,46 @@
+
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+
+// const infuraKey = "fj4jll3k.....";
+//
+const { mnemonic, bscScanApiKey} = require('./env.json');
+
+module.exports = {
+
+  plugins: [
+    'truffle-plugin-verify'
+  ],
+  api_keys: {
+    bscscan: bscScanApiKey
+  },
+  networks: {
+
+    testnet: {
+        provider: () => new HDWalletProvider(mnemonic, `https://data-seed-prebsc-1-s1.binance.org:8545`),
+        network_id: 97,
+        timeoutBlocks: 200,
+        confirmations: 5,
+        production: true    // Treats this network as if it was a public net. (default: false)
+    }
+  },
+
+  // Set default mocha options here, use special reporters etc.
+  mocha: {
+    timeout: 100000
+  },
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+       version: "0.5.16",    // Fetch exact version from solc-bin (default: truffle's version)
+      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+      settings: {          // See the solidity docs for advice about optimization and evmVersion
+       optimizer: {
+         enabled: false,
+         runs: 200
+       },
+       evmVersion: "byzantium"
+      }
+    },
+  },
+};
---
+++ test2.sol
@@ -0,0 +1,218 @@
+
+pragma solidity ^0.8.0;
+
+// import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol";
+// import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC1155/ERC721.sol";
+// import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/math/SafeMath.sol";
+// import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+// import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol';
+import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol';
+import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol';
+import 'https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Counters.sol';
+
+
+// mint id 0 at 10bnb
+// mint id 1-500 at 0.3 bnb
+
+
+contract THC_NAZA is ERC721, Ownable {
+    uint256 public numTokens = 0;
+    uint256 public nonce;
+    uint256[] public indices;
+    uint256 public constant THC_NAZA_SUPPLY = 501;
+    // uint id = 0;
+
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIdentifiers;
+
+    // Optional mapping for token URIs
+    mapping (uint256 => string) private _tokenURIs;
+
+    // Base URI
+    string private _baseURIextended;
+
+
+    function setBaseURI(string memory baseURI_) external onlyOwner() {
+        _baseURIextended = baseURI_;
+    }
+
+    function _setTokenURI(uint256 tokenId, string memory _tokenURI) internal virtual {
+        require(_exists(tokenId), "ERC721Metadata: URI set of nonexistent token");
+        _tokenURIs[tokenId] = _tokenURI;
+    }
+
+    function _baseURI() internal view virtual override returns (string memory) {
+        return _baseURIextended;
+    }
+
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+
+        string memory _tokenURI = _tokenURIs[tokenId];
+        string memory base = _baseURI();
+        string memory filetype = ".json";
+
+        // If there is no base URI, return the token URI.
+        if (bytes(base).length == 0) {
+            return _tokenURI;
+        }
+        // If both are set, concatenate the baseURI and tokenURI (via abi.encodePacked).
+        if (bytes(_tokenURI).length > 0) {
+            return string(abi.encodePacked(base, _tokenURI, filetype));
+        }
+        // If there is a baseURI but no tokenURI, concatenate the tokenID to the baseURI.
+        // return string(abi.encodePacked(base, tokenId);
+    }
+
+
+
+    function _mintGenesis(address _to) public returns (uint) {
+        // uint id = randomIndex();
+        numTokens = numTokens + 1;
+        string memory tokenURITest = "hello";
+        // uint256 idtest = 0;
+        _mint(_to, 0);
+        _setTokenURI(0,"0");
+        // _setTokenURI(idtest, tokenURITest);
+        // ++id;
+        // return id;
+    }
+
+
+    function _mint(address _to) public {
+        // uint id = randomIndex();
+        numTokens = numTokens + 1;
+        string memory tokenURITest = "hello";
+        // uint256 idtest = 0;
+        // _mint(_to, id);
+        // _setTokenURI(id,"0");
+        // _setTokenURI(idtest, tokenURITest);
+        // ++id;
+        // return id;
+        // Incrementing ID to create new tokenn
+        _tokenIdentifiers.increment();
+        // _mint(_to, tokenId);
+        uint256 newRECIdentifier = _tokenIdentifiers.current();
+        // _setTokenURI(id, uint2str(newRECIdentifier));
+    }
+
+    /**
+        * @dev Mints 1 NFT
+    */
+    function mintNFT() public payable {
+        // require(totalSupply() < MAX_NFT_SUPPLY, "Sale has already ended");
+        // require(getNFTPrice() == msg.value, "Ether value sent is not correct");
+
+        // Incrementing ID to create new tokenn
+        // _tokenIdentifiers.increment();
+        // uint256 newRECIdentifier = _tokenIdentifiers.current();
+
+        // uint tokenId = 0;
+        // string memory _tokenURI = "hello";
+        // string memory tokenURITest = "hello";
+        // _safeMint(msg.sender, 0);
+        // string memory id = uint2str(tokenId);
+        // _setTokenURI(0,id);
+        // ++tokenId;
+
+        // Incrementing ID to create new tokenn
+        _tokenIdentifiers.increment();
+        // _safeMint(msg.sender, tokenId);
+        // uint256 newRECIdentifier = _tokenIdentifiers.current();
+                uint256 newRECIdentifier = _tokenIdentifiers.current();
+        // _safeMint(msg.sender, newRECIdentifier);
+        //   _setTokenURI(newRECIdentifier, tokenURI(1));
+
+
+        //RNG
+        // uint256 newRECIdentifier = uint(blockhash(block.number - 1)) % THC_NAZA_SUPPLY - numTokens;
+
+        //TODO - REVIEW we use tokenID twice.. but token ID should match URI anyways
+        _safeMint(msg.sender, newRECIdentifier);
+        string memory uid = uint2str(newRECIdentifier);
+        _setTokenURI(newRECIdentifier, uid);
+    }
+
+
+    constructor() ERC721("TranshumanCoin", "THC") {
+        _baseURIextended = "https://gateway.pinata.cloud/ipfs/QmYipxb1MvM6DF4NcVRzGMhxPRyWDXWXAwneGGovi4Khdp/THC_NAZA_";
+        uint tokenId = 0;
+        // string memory baseURI_ = _baseURIextended;
+    }
+
+    function uint2str(uint _i) internal pure returns (string memory _uintAsString) {
+        if (_i == 0) {
+            return "0";
+        }
+        uint j = _i;
+        uint len;
+        while (j != 0) {
+            len++;
+            j /= 10;
+        }
+        bytes memory bstr = new bytes(len);
+        uint k = len;
+        while (_i != 0) {
+            k = k-1;
+            uint8 temp = (48 + uint8(_i - _i / 10 * 10));
+            bytes1 b1 = bytes1(temp);
+            bstr[k] = b1;
+            _i /= 10;
+        }
+        return string(bstr);
+    }
+
+    function randomIndex() internal returns (uint) {
+        uint totalSize = THC_NAZA_SUPPLY - numTokens;
+        uint index = uint(keccak256(abi.encodePacked(nonce, msg.sender, block.difficulty, block.timestamp))) % totalSize;
+        uint value = 0;
+        if (indices[index] != 0) {
+            value = indices[index];
+        } else {
+            value = index;
+        }
+
+        if (indices[totalSize - 1] == 0) {
+
+            indices[index] = totalSize - 1;
+        } else {
+            indices[index] = indices[totalSize - 1];
+        }
+        nonce++;
+        return value;
+    }
+}
+
+
+
+//         /**
+//      * @dev Set the base URI
+//      */
+//     function setBaseURI(string memory baseURI_) external onlyOwner() {
+//         _baseURIextended = baseURI_;
+//     }
+// }
+
+//     /**
+//     * @dev Pause
+//     */
+//     function pause() external virtual onlyOwner() {
+//         super._pause();
+//     }
+
+//   /**
+//     * @dev Unpause
+//     */
+//     function unpause() external virtual onlyOwner() {
+//         super._unpause();
+//     }
+// }
+
+
+
+
+
+
+// constructor() ERC1155("https://gateway.pinata.cloud/ipfs/QmS7vBvBqbdcUm3zx3cwkUc9hiFp4eqSW7xGoB5Wv6dzzj/THC_NAZA_={id}.json") {
+//     _mint(msg.sender, 1, 100, "" );
+// }
